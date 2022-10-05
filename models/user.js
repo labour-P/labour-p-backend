93e2b1d056f62a24add84658aefb98bbf77c6915
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-10-05 12:33:38 +0100

    update

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const usersSchema = mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
-  username: { type: String, required: true },
+  userName: { type: String, required: true },
   password: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
