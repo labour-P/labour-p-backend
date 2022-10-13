ccea8fddd10d4d9f8ef6a9e98065b954ef067568
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-10-13 17:02:59 +0100

    update

--- controllers/posts.js
+++ controllers/posts.js
@@ -32,7 +32,7 @@ export const viewComments = asyncWrapper(async (req, res) => {
     thread}= req.body
 
     try {
-      Comments.findOne({ thread: thread }, function (err, response) {
+      Comments.find({ thread: thread }, function (err, response) {
         if(!response){
           return res
           .status(400)
