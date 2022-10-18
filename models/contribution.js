570acf77b81ff83d55d73b2633b49a87c38239d6
samuel ibitoye <98920110+samuel-ibits@users.noreply.github.com>
2022-10-18 12:32:29 +0100

    update

--- models/contribution.js
+++ models/contribution.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const contributionSchema = mongoose.Schema({
-  userid: { type: String, required: true },
-  type: { type: String, required: true },
-  location: { type: String, required: true },
-  date: { type: String, required: true },
-  time: { type: String, required: true },
-  other: { type: String, required: true },
+  userid: { type: String },
+  type: { type: String },
+  location: { type: String },
+  date: { type: String },
+  time: { type: String },
+  other: { type: String },
   
 });
 
